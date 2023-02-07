f3dbe33690282822f7209fda3d705d0db7d4d67b
qianzuo <qianzuo612@foxmail.com>
2023-02-07 10:03:34 +0800

    update url

--- vue.config.js
+++ vue.config.js
@@ -36,7 +36,17 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
+    //before: require('./mock/mock-server.js')
+    proxy: {
+      '^/api': {
+          target: 'http://localhost:9000/',//接口的前缀
+          ws:true,//代理websocked
+          changeOrigin:true,//虚拟的站点需要更管origin
+          pathRewrite:{
+              '^/api':''//重写路径
+          }
+      }
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
